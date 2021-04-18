7ce3c46974c035c5781bd83a88ab04b64b2daa33
azhardhiaulhaq <azhar@tantowi.com>
2021-04-18 17:13:48 +0700

    Relation Classification

--- modules/RelationClassification.py
+++ modules/RelationClassification.py
@@ -175,4 +175,4 @@ relation_classification = RelationClassification()
 #                                  'makanan tidak enak, kamar sempit dan kotor. kamar tidak enak',
 #                                  'makanan tidak enak, kamar sempit dan kotor. kamar sempit',
 #                                  'makanan tidak enak, kamar sempit dan kotor. kamar kotor'])
-# relation_classification.train()
+relation_classification.train()
